b103f095d39c5876abc0baea027bb401d1b4f5fb
Victor Amin <victor.amin@gmail.com>
2014-07-13 14:18:48 -0500

    set headers to false for subset

--- plot1.R
+++ plot1.R
@@ -5,14 +5,14 @@ power.consumption.100 <- read.table("data/household_power_consumption.txt",
                                     stringsAsFactors = FALSE, 
                                     nrows = 100)
 col.names <- colnames(power.consumption.100)
-classes <- sapply(power.consumption.100, class)
+col.classes <- sapply(power.consumption.100, class)
 # read in data for 1/2/2007 and 2/2/2007
 power.consumption <- read.table(pipe('grep "^[1-2]/2/2007" "data/household_power_consumption.txt"'), 
-                                header = TRUE, 
+                                header = FALSE, 
                                 sep = ";",
                                 na.strings = c("?"), 
                                 col.names = col.names, 
-                                colClasses = classes)
+                                colClasses = col.classes)
 # plot graph
 png(file = "plot1.png")
 hist(power.consumption$Global_active_power, 
